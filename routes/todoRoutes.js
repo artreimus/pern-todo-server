ae14cbbe6c5a10060cd3a2feb09f4cb53c9bc921
artreimus <63195930+artreimus@users.noreply.github.com>
2023-03-02 16:41:34 +0800

    add require auth middleware, and get list todo route

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -5,11 +5,15 @@ import {
   getTodo,
   updateTodo,
   deleteTodo,
+  getListTodos,
 } from '../controllers/todoController.js';
+import verifyJWT from '../middlewares/verifyJWT.js';
 
 const router = express.Router();
 
+router.use(verifyJWT);
 router.route('/').post(createTodo).get(getAllTodos);
 router.route('/:id').get(getTodo).patch(updateTodo).delete(deleteTodo);
+router.route('/list/:id').get(getListTodos);
 
 export default router;
