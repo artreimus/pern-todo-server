faa058601629e20b300a4ef28a7656b311bad963
artreimus <63195930+artreimus@users.noreply.github.com>
2023-03-02 16:35:58 +0800

    refactor, and add get list todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,6 @@
 import asyncHandler from 'express-async-handler';
 import pool from '../db/index.js';
-import { NotFoundError } from '../error/index.js';
+import CustomError from '../error/index.js';
 
 // @desc GET all to dos
 // @route /api/v1/todos
@@ -18,8 +18,17 @@ const getTodo = asyncHandler(async (req, res) => {
   const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [id]);
 
   if (!todo.rows.length) {
-    throw new NotFoundError(`Todo with id ${id} was not found`);
+    throw new CustomError.NotFoundError(`Todo with id ${id} was not found`);
   }
+  res.status(200).json({ data: todo.rows });
+});
+
+// @desc GET all the todos in a list
+// @route /api/v1/todos/list/:id
+// @access public
+const getListTodos = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const todo = await pool.query('SELECT * FROM todo WHERE list_id = $1', [id]);
   res.status(200).json({ data: todo.rows });
 });
 
@@ -27,11 +36,20 @@ const getTodo = asyncHandler(async (req, res) => {
 // @route /api/v1/todos
 // @access public
 const createTodo = asyncHandler(async (req, res) => {
-  const { description } = req.body;
-  const newTodo = await pool.query(
-    'INSERT INTO todo (description) VALUES($1) RETURNING *',
-    [description]
-  );
+  const { description, due_date, list_id } = req.body;
+  let newTodo;
+  if (due_date) {
+    newTodo = await pool.query(
+      'INSERT INTO todo (description, due_date, list_id) VALUES($1, $2, $3) RETURNING *',
+      [description, due_date, list_id]
+    );
+  } else {
+    newTodo = await pool.query(
+      'INSERT INTO todo (description, list_id) VALUES($1, $2) RETURNING *',
+      [description, list_id]
+    );
+  }
+
   res.status(200).json({ success: true, data: newTodo.rows[0] });
 });
 
@@ -40,10 +58,11 @@ const createTodo = asyncHandler(async (req, res) => {
 // @access public
 const updateTodo = asyncHandler(async (req, res) => {
   const { id } = req.params;
-  const { description } = req.body;
+  const { description, completed, due_date } = req.body;
+
   const updatedTodo = await pool.query(
-    'UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *',
-    [description, id]
+    'UPDATE todo SET description = $1, completed= $2, due_date = $3 WHERE todo_id = $4 RETURNING *',
+    [description, completed, due_date, id]
   );
   res.status(200).json({ success: true, data: updatedTodo.rows });
 });
@@ -60,4 +79,11 @@ const deleteTodo = asyncHandler(async (req, res) => {
   res.status(200).json({ success: true, data: deletedTodo.rows });
 });
 
-export { createTodo, getAllTodos, getTodo, updateTodo, deleteTodo };
+export {
+  createTodo,
+  getAllTodos,
+  getTodo,
+  updateTodo,
+  deleteTodo,
+  getListTodos,
+};
